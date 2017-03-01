b475129b0b7d4c06b19875809e3f61f594674cdf
Hamilton White <dev.centaurius@gmail.com>
2017-03-01 22:25:52 +0000

    static weather autoupdate 2017-03-01-22:25:52

--- clean.sh
+++ clean.sh
@@ -13,7 +13,7 @@ cd /home/ubuntu/workspace/staticweatherPull/wind
 rm *.jpg
 
 cd /home/ubuntu/workspace/staticweatherPull
-sudo mv cloud.zip /zips
-sudo mv precipitation.zip /zips
-sudo mv wind.zip /zips
-sudo mv temp.zip /zips
+sudo mv cloud.zip zips
+sudo mv precipitation.zip zips
+sudo mv wind.zip zips
+sudo mv temp.zip zips
